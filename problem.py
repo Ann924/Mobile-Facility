69f5656b35c4c152f3b469150a4fd1b1bb358fb2
al7gc@virginia.edu <al7gc@virginia.edu>
2021-07-21 19:55:19 -0400

    rounding + k-center

--- problem.py
+++ problem.py
@@ -10,9 +10,10 @@ from ortools.linear_solver.pywraplp import Variable, Constraint, Objective
 #index = index of the individual
 #location = Node representing location visited by that individual
 #facility = Node at which a facility can be placed
-address = namedtuple('address', ['index', 'location', 'facility'])
-
 #TODO: include more functions to get variables back, etc.
+#TODO: will clean up class structures
+
+address = namedtuple('address', ['index', 'location', 'facility'])
 
 '''
 Data input:
@@ -22,12 +23,12 @@ Data input:
 '''
 
 class LP:
-    def __init__(self, G:List[List[float]], client_locations: List[List[int]], k: int):
+    def __init__(self, G:List[List[float]], client_locations: List[List[int]], k: int, solver_id = "GLOP"):
         self.G = G
         self.client_locations = client_locations
         self.k = k
         
-        self.solver = pywraplp.Solver.CreateSolver('GLOP')
+        self.solver = pywraplp.Solver.CreateSolver(solver_id)
         
         self.init_variables()
         self.init_constraints()
@@ -53,7 +54,7 @@ class LP:
         
         self.w = self.solver.NumVar(0, self.solver.infinity(), 'w')
         
-        print('Number of variables =', self.solver.NumVariables())
+        #print('Number of variables =', self.solver.NumVariables())
 
     #TODO: Add constraint to avoid opening a facility at a house location?
     def init_constraints(self):
@@ -81,7 +82,7 @@ class LP:
             #Making sure assignment follow open facilities
             self.solver.Add(self.Y[address] <= self.X[address.facility])
         
-        print('Number of constraints =', self.solver.NumConstraints())
+        #print('Number of constraints =', self.solver.NumConstraints())
     
     #K-center objective to minimize the maximum distance
     def init_objective(self):
@@ -95,10 +96,9 @@ class LP:
         self.status = self.solver.Solve()
         
         if self.status == pywraplp.Solver.OPTIMAL:
-            print('Solution:')
             print('Objective value =', self.objective.Value())
 
-            print("X VALUES")
+            '''print("X VALUES")
             for ind in range(len(self.X)):
                 if self.X[ind].solution_value()>0:
                     print(str(ind) + "\t" + str(self.X[ind].solution_value()))
@@ -106,7 +106,7 @@ class LP:
             print("Y VALUES")
             for address in self.Y.keys():
                 if self.Y[address].solution_value()>0:
-                    print(str(address) + "\t" + str(self.Y[address].solution_value()))
+                    print(str(address) + "\t" + str(self.Y[address].solution_value()))'''
 
             print('Problem solved in %f milliseconds' % self.solver.wall_time())
             print('Problem solved in %d iterations' % self.solver.iterations())
@@ -115,7 +115,7 @@ class LP:
     
     def get_variable_solution(self):
         if self.status == pywraplp.Solver.OPTIMAL:
-            return [self.X[ind].solution_value() for ind in range(len(self.X))], [self.Y[address].solution_value() for address in self.Y.keys()]
+            return [self.X[ind].solution_value() for ind in range(len(self.X))], {address: self.Y[address].solution_value() for address in self.Y.keys()}
         else:
             print("Not optimal")
             return [], []
@@ -126,20 +126,10 @@ class LP:
         else:
             print("Not optimal")
             return -1
-
-
         
-class MILP:
-    def __init__(self, G:List[List[float]], client_locations: List[List[int]], k: int):
-        self.G = G
-        self.client_locations = client_locations
-        self.k = k
-        
-        self.solver = pywraplp.Solver.CreateSolver('SCIP')
-        
-        self.init_variables()
-        self.init_constraints()
-        self.init_objective()
+class MILP(LP):
+    def __init__(self, G:List[List[float]], client_locations: List[List[int]], k: int, solver_id = 'SCIP'):
+        super().__init__(G, client_locations, k, solver_id)
     
     '''
     X is the indicator variable for whether a facility is open
@@ -161,7 +151,44 @@ class MILP:
         
         self.w = self.solver.NumVar(0, self.solver.infinity(), 'w')
         
-        print('Number of variables =', self.solver.NumVariables())
+        #print('Number of variables =', self.solver.NumVariables())
+
+'''
+clients is a list of the locations at which clients are located
+'''
+assignment = namedtuple('assignment', ['location', 'facility'])
+
+class K_LP:
+    def __init__(self, G:List[List[float]], clients: List[int], k: int, solver_id = "GLOP"):
+        self.G = G
+        self.clients = clients
+        self.k = k
+        
+        self.solver = pywraplp.Solver.CreateSolver(solver_id)
+        
+        self.init_variables()
+        self.init_constraints()
+        self.init_objective()
+    
+    '''
+    X is the indicator variable for whether a facility is open
+    Y is the indicator variable to assign an individual from one of the travelled locations to the nearest facility
+    '''
+    def init_variables(self):
+        #Set indicator variables for indicating whether a facility is open
+        self.X: Dict[int, Variable] = {}
+        for node in range(len(self.G)):
+            self.X[node] = self.solver.NumVar(0, 1, f"x_{node}")
+        
+        #Set indicator variables for indicating an individual's assignment to a location and facility
+        self.Y: Dict[Tuple[int, int], Variable] = {}
+        for client in self.clients:
+            for node in set(range(len(self.G))):
+                self.Y[assignment(client, node)] = self.solver.NumVar(0, 1, f"y_{client, node}")
+        
+        self.w = self.solver.NumVar(0, self.solver.infinity(), 'w')
+        
+        #print('Number of variables =', self.solver.NumVariables())
 
     #TODO: Add constraint to avoid opening a facility at a house location?
     def init_constraints(self):
@@ -171,25 +198,19 @@ class MILP:
         for ind in self.X.keys():
             self.budget.SetCoefficient(self.X[ind], 1)
         
-        '''#Setting the constraint for no open facilities at homes
-        self.home = self.solver.Constraint(0, 0, 'home')
-        for location_list in self.client_locations:
-            self.home.SetCoefficient(self.X[location_list[0]], 1)'''
-        
         #Assigning each person to only one facility
-        for ind in range(len(self.client_locations)):
+        for client in self.clients:
             person_limit: Constraint = self.solver.Constraint(1, 1, 'person_limit')
-            for address in self.Y.keys():
-                if address.index == ind:
-                    person_limit.SetCoefficient(self.Y[address], 1)
+            for node in range(len(self.G)):
+                person_limit.SetCoefficient(self.Y[assignment(client, node)], 1)
         
-        for address in self.Y.keys():
+        for assign in self.Y.keys():
             #Finding maximum assignment cost
-            self.solver.Add(self.w >= self.Y[address] * cost(self.G, address.location, address.facility))
+            self.solver.Add(self.w >= self.Y[assign] * cost(self.G, assign.location, assign.facility))
             #Making sure assignment follow open facilities
-            self.solver.Add(self.Y[address] <= self.X[address.facility])
+            self.solver.Add(self.Y[assign] <= self.X[assign.facility])
         
-        print('Number of constraints =', self.solver.NumConstraints())
+        #print('Number of constraints =', self.solver.NumConstraints())
     
     #K-center objective to minimize the maximum distance
     def init_objective(self):
@@ -203,18 +224,17 @@ class MILP:
         self.status = self.solver.Solve()
         
         if self.status == pywraplp.Solver.OPTIMAL:
-            print('Solution:')
             print('Objective value =', self.objective.Value())
 
-            print("X VALUES")
+            '''print("X VALUES")
             for ind in range(len(self.X)):
                 if self.X[ind].solution_value()>0:
                     print(str(ind) + "\t" + str(self.X[ind].solution_value()))
 
             print("Y VALUES")
-            for address in self.Y.keys():
-                if self.Y[address].solution_value()>0:
-                    print(str(address) + "\t" + str(self.Y[address].solution_value()))
+            for assign in self.Y.keys():
+                if self.Y[assign].solution_value()>0:
+                    print(str(assign) + "\t" + str(self.Y[assign].solution_value()))'''
 
             print('Problem solved in %f milliseconds' % self.solver.wall_time())
             print('Problem solved in %d iterations' % self.solver.iterations())
@@ -223,7 +243,7 @@ class MILP:
     
     def get_variable_solution(self):
         if self.status == pywraplp.Solver.OPTIMAL:
-            return [self.X[ind].solution_value() for ind in range(len(self.X))], [self.Y[address].solution_value() for address in self.Y.keys()]
+            return [self.X[ind].solution_value() for ind in range(len(self.X))], {assign: self.Y[assign].solution_value() for assign in self.Y.keys()}
         else:
             print("Not optimal")
             return [], []
@@ -234,3 +254,27 @@ class MILP:
         else:
             print("Not optimal")
             return -1
+
+class integer_K_LP(K_LP):
+    def __init__(self, G:List[List[float]], clients: List[int], k: int, solver_id = 'SCIP'):
+        super().__init__(G, clients, k, solver_id)
+    
+    '''
+    X is the indicator variable for whether a facility is open
+    Y is the indicator variable to assign an individual from one of the travelled locations to the nearest facility
+    '''
+    def init_variables(self):
+        #Set indicator variables for indicating whether a facility is open
+        self.X: Dict[int, Variable] = {}
+        for node in range(len(self.G)):
+            self.X[node] = self.solver.IntVar(0, 1, f"x_{node}")
+        
+        #Set indicator variables for indicating an individual's assignment to a location and facility
+        self.Y: Dict[Tuple[int, int], Variable] = {}
+        for client in self.clients:
+            for node in set(range(len(self.G))):
+                self.Y[assignment(client, node)] = self.solver.IntVar(0, 1, f"y_{client, node}")
+        
+        self.w = self.solver.NumVar(0, self.solver.infinity(), 'w')
+        
+        #print('Number of variables =', self.solver.NumVariables())
