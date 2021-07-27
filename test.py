b25338e0795cf0a9307e94e7ab8a2f49c311fa5c
al7gc@virginia.edu <al7gc@virginia.edu>
2021-07-27 14:06:47 -0400

    update

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from utils import *
 #Possible heuristics: independent rounding, dependent rounding, dispersion (Madhav's version), k-median (w/ houses), high traffic areas
 #TODO: test more distances and costs
 
-def test_line(number_of_points:int, distance_range:Tuple[float, float]):
+def test_line(number_of_points:int, distance_range:Tuple[float, float]) -> List[List[float]]:
     """
     number_of_points > 0
     """
@@ -38,10 +38,66 @@ def generate_random_input(number_of_points:int, distance_range: Tuple[float, flo
     
     return G
 
+def generate_2D_input(number_of_points:int, x_coordinate_range: Tuple[float, float], y_coordinate_range: Tuple[float, float]) -> List[List[float]]:
+    
+    points = [(random.random()*(x_coordinate_range[1]-x_coordinate_range[0]) + x_coordinate_range[0], random.random()*(y_coordinate_range[1]-y_coordinate_range[0]) + y_coordinate_range[0]) for i in range(number_of_points)]
+    #print(points)
+    G = []
+    for r in range(number_of_points):
+        G.append([])
+        for c in range(r):
+            loc0 = points[r]
+            loc1 = points[c]
+            distance = ((loc0[0]-loc1[0])**2 + (loc0[1]-loc1[1])**2)**(1/2)
+            G[-1].append(distance)
+        G[-1].append(0)
+    
+    return G
+
+def contains_float(X):
+    
+    for val in X:
+        if not val.is_integer(): return True
+    return False
+
+def lp_experiment(G, client_locations, k):
+    #Solves the relaxed k-center linear program for multiple client locations
+    my_lp = LP(G, client_locations, k)
+    my_lp.solve_lp()
+    X, Y = my_lp.get_variable_solution()
+    print(X)
+    
+    fixed_variables = set()
+    while contains_float(X):
+    #Independently rounds on X, then reassigns Y according to X
+        epsilon = 0.1
+        X_important = [i for i in range(len(X)) if i not in fixed_variables and (X[i]>1-epsilon or X[i]<epsilon)]
+
+        while len(X_important) == 0:
+            epsilon += 0.1
+            X_important = [i for i in range(len(X)) if i not in fixed_variables and (X[i]>1-epsilon or X[i]<epsilon)]
+        
+        for var in X_important:
+            fixed_variables.add(var)
+            val = round(X[var])
+            my_lp.set_variable(var, val)
+
+        my_lp.solve_lp()
+        X, Y = my_lp.get_variable_solution()
+        print(X)
+    
+    facilities = [ind for ind in range(len(X)) if X[ind]==1]
+    assignments = assign_facilities(G, client_locations, facilities)
+    format_location_output(facilities, assignments)
+    print("Recalculated Objective Value: \t" + str(calculate_objective(G, assignments)))
+    
+    return facilities, assignments
+
 def test_function(number_of_points:int, distance_range:Tuple[float, float], number_of_clients:Tuple[int, int], number_of_visits: Tuple[int, int], k:int):
     
     print("-------------------G----------------------")
-    G = test_line(number_of_points, distance_range)
+    #G = test_line(number_of_points, distance_range)
+    G = generate_2D_input(number_of_points, (-10, 10), (-10, 10))
     for row in G:
         print(['{:.2f}'.format(i) for i in row])
     
@@ -82,4 +138,7 @@ def test_function(number_of_points:int, distance_range:Tuple[float, float], numb
     format_location_output(X_center, Y_center)
     print("Recalculated Objective Value: \t" + str(calculate_objective(G, Y_center)))
 
-test_function(5, (1,1), (1,4), (2,4), 1)
+X, Y = lp_experiment([[0],[1, 0],[2, 1, 0]], [[2],[1, 0]], 1)
+
+#test_function(5, (1,1), (1,4), (2,4), 1)
+#print(generate_2D_input(3, (-1, 1), (-1, 1)))
