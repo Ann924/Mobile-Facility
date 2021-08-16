b0c686873a39129c5b40f959ab6f302d398415f0
Ann924 <al7gc@virginia.edu>
2021-08-16 19:53:00 -0400

    aggregation and experiments

--- config.py
+++ config.py
@@ -14,6 +14,7 @@ HOME_SHIFT : To adjust the lids of residential locations
 
 LOCATIONS : List containing dicts of the following
     "lid" : The lid (with applied HOME_SHIFT) of a given location
+    "lid_ind" : The index of each location in LOCATIONS
     "longitude"
     "latitude"
     "activity": The number of clients that visit this location
@@ -22,14 +23,15 @@ LOCATIONS : List containing dicts of the following
 CLIENT_LOCATIONS : Dict mapping pids of clients to the locations (represented by location indicies from LOCATIONS) each client visits
 
 LOCATIONS_agg : List of dicts of the aggregated locations (within radius 10 m)
-    "lid" : the index of the lid (from LOCATIONS) of the center of the aggregated location
-    "longitude" : longitude of the center
-    "latitude" : latitude of the center
-    "members" : the lid indices that belong to this cluster
-    "activity" : the number of clients that visit this aggregated cluster of locations
-    "pid" : the list of clients that visit this aggregated cluster of locations
+    "lid" : The index of the lid (from LOCATIONS) of the center of the aggregated location
+    "lid_ind" : The index of each location in LOCATIONS
+    "longitude" : Longitude of the center
+    "latitude" : Latitude of the center
+    "members" : The lid indices that belong to this cluster
+    "activity" : The number of clients that visit this aggregated cluster of locations
+    "pid" : The list of clients that visit this aggregated cluster of locations
 
-CLIENT_LOCATIONS_agg : Dict mapping pids of clients to the aggregated visited locations
+CLIENT_LOCATIONS_agg : Dict mapping pids of clients to the aggregated locations
 ---------
 
 """
@@ -148,9 +150,20 @@ def read_data_input(filename):
     CLIENT_LOCATIONS = {int(value['pid']): value['lid'] for ind, value in data["CLIENT_LOCATIONS"].items()}
     return LOCATIONS, CLIENT_LOCATIONS
 
+######################################################
+
 LOCATIONS, CLIENT_LOCATIONS = read_data_input("data2.json")
 
+######################################################
+
 def radius_cover(radius: float):
+    """
+    Helper method for set_cover_aggregation
+    
+    Generates balls for the given radius
+    Stores chosen locations and radius-coverage mappings in a json file
+    """
+    
     radius_dict = {}
     LOCATIONS_act = [(ind, value) for ind,value in enumerate(LOCATIONS) if LOCATIONS[ind]['lid']<HOME_SHIFT]
     
@@ -184,115 +197,13 @@ def radius_cover(radius: float):
         data = {"radius": radius, "radius_dict": radius_dict, "chosen": list(chosen)}
         json.dump(data, f)
 
-def new_data(radius: float = 0.01):
+def set_cover_aggregation(radius: float = 0.01):
     """
-    must be run after LOCATIONS and CLIENT_LOCATIONS are read int
-    creates a version of LOCATIONS and CLIENT_LOCATIONS that are based on the aggregate locs
-    """
-    """file_dict = open("radius_cluster.json", 'r')
-    data_dict = json.load(file_dict)
-    cluster_dict = {int(ind): val for ind, val in data_dict["radius_dict"].items()}
+    Must be run after original LOCATIONS and CLIENT_LOCATIONS are read in
     
-    file_chosen = open("radius_cover.json", 'r')
-    data_chosen = json.load(file_chosen)
-    chosen_points = set(data_chosen["chosen"])"""
-    
-    file_radius = open(f"radius_cover_{radius}.json", 'r')
-    data_radius = json.load(file_radius)
-    cluster_dict = {int(ind): val for ind, val in data_radius["radius_dict"].items()}
-    chosen_points = set(data_radius["chosen"])
-    
-    LOCATIONS_act = [(ind, value) for ind,value in enumerate(LOCATIONS) if LOCATIONS[ind]['lid']<HOME_SHIFT]
-    
-    reverse_lid_index = {}
-    for i, loc in enumerate(LOCATIONS_act):
-        reverse_lid_index[loc[0]] = i
-    
-    temp = []
-    cluster_dict_single = {}
-    
-    #To avoid repeat coverage, assign aggregation centers (from radius_dict) to the closest location in their set
-    min_matching = {}
-    for cluster in cluster_dict.keys():
-        
-        min_center = (float('inf'), -1)
-        
-        for member in cluster_dict[cluster]:
-            
-            if member in chosen_points:
-                coord1_row = LOCATIONS[cluster]
-                coord2_row = LOCATIONS[member]
-                coord1 = (coord1_row['latitude'], coord1_row['longitude'])
-                coord2 = (coord2_row['latitude'], coord2_row['longitude'])
-                dist = geopy.distance.great_circle(coord1, coord2).km
-                #calculate_distance(cluster, member)
-
-                if dist < min_center[0]:
-                    min_center = (dist, member)
-        
-        min_matching[cluster] = min_center[1]
-    
-    for point in chosen_points:
-        
-        pid_set = set()
-        member_list = []
-        
-        for loc in cluster_dict[point]:
-            
-            if min_matching[loc] == point:
-                member_list.append(loc)
-                pid_set = pid_set.union(LOCATIONS[loc]['pid'])
-        
-        temp.append({"lid_ind": point,
-                     #"lid": LOCATIONS[point]['lid'],
-                     #"longitude": LOCATIONS[point]['longitude'] ,
-                     #"latitude": LOCATIONS[point]['latitude'],
-                     "members": member_list,
-                     "activity": len(pid_set),
-                     "pid": list(pid_set)})
-    
-    temp_res = []
-    LOCATIONS_res = [(ind, value) for ind,value in enumerate(LOCATIONS) if LOCATIONS[ind]['lid'] > HOME_SHIFT]
-    
-    for loc in LOCATIONS_res:
-        
-        locations_dict = LOCATIONS[loc[0]]
-        
-        new_dict = {}
-        new_dict["lid_ind"] = loc[0]
-        new_dict["lid"] = locations_dict['lid']
-        new_dict["longitude"] = locations_dict['longitude']
-        new_dict["latitude"] = locations_dict['latitude']
-        new_dict["members"] = [loc[0]]
-        new_dict["activity"] = locations_dict['activity']
-        new_dict["pid"] = locations_dict["pid"]
-        temp_res.append(new_dict)
-    
-    temp = sorted(temp+temp_res, key = lambda x: x["activity"], reverse = True)
-    
-    #-----Now for clients----#
-    
-    temp_client = {}
-    for key, val in CLIENT_LOCATIONS.items():
-        new_lid_list = []
-        for loc in val:
-            
-            if loc not in chosen_points and LOCATIONS[loc]['lid'] < HOME_SHIFT:
-                #print(loc)
-                if min_matching[loc] not in new_lid_list:
-                    new_lid_list.append(min_matching[loc])
-            else:
-                new_lid_list.append(loc)
-        
-        temp_client[key] = new_lid_list
-    
-    return temp, temp_client
-
-def new_data_repeats(radius: float = 0.01):
-    """
-    must be run after LOCATIONS and CLIENT_LOCATIONS are read int
-    creates a version of LOCATIONS and CLIENT_LOCATIONS that are based on the aggregate locs
-    locations that are covered by multiple aggregation centers will be selected to attend all
+    Creates balls of given radius around each location
+    Picks location balls to cover all possible facility locations through set cover approximation
+    Creates and returns a version of LOCATIONS and CLIENT_LOCATIONS that are based on the aggregate locs
     """
     
     file_radius = open(f"radius_cover_{radius}.json", 'r')
@@ -367,16 +278,171 @@ def new_data_repeats(radius: float = 0.01):
     
     return temp, temp_client
 
-def aggregate_data(aggregation: int = 1, radius: float = 0.01):
+#TODO: incorporate pipelines
+def single_linkage_aggregation(radius: float = 0.02):
+    """
+    Must be run after LOCATIONS and CLIENT_LOCATIONS are read in
     
+    
+    Returns aggregated versions of LOCATIONS and CLIENT_LOCATIONS with single linkage clustering
+    """
+    
+    file_dict = open("aid2pid.json", 'r')
+    data_dict = json.load(file_dict)
+    AID_LOC = data_dict['data']
+
+    file_client = open("pid2aid.json", 'r')
+    data_client = json.load(file_client)
+    AID_CLIENT = {int(key):val for key,val in data_client['data'].items()}
+    
+    lid_to_ind = {}
+    for ind, val in enumerate(LOCATIONS):
+        lid_to_ind[val['lid']] = ind
+    
+    ind_to_aid = {}
+    LOCATIONS_agg = []
+    original_loc_length = len(LOCATIONS)
+    
+    count = 0
+    for i in range(len(AID_LOC)):
+        
+        if AID_LOC[i]['aid'] >= HOME_SHIFT:
+            new_dict = {}
+            new_dict["lid_ind"] = lid_to_ind[AID_LOC[i]['aid']]
+            new_dict["lid"] = AID_LOC[i]['aid']
+            new_dict["longitude"] = AID_LOC[i]['longitude']
+            new_dict["latitude"] = AID_LOC[i]['latitude']
+            new_dict["members"] = [lid_to_ind[AID_LOC[i]['aid']]]
+            new_dict["activity"] = AID_LOC[i]["activity"]
+            new_dict["pid"] = AID_LOC[i]["visitors"]
+            
+            ind_to_aid[lid_to_ind[AID_LOC[i]['aid']]] = i
+        
+        else:
+            #lids of new aggregated locations are set to be negative
+            new_dict = {}
+            new_dict["lid_ind"] = original_loc_length + count
+            new_dict["lid"] = -1 * (count+1)
+            new_dict["longitude"] = AID_LOC[i]['longitude']
+            new_dict["latitude"] = AID_LOC[i]['latitude']
+            new_dict["members"] = [lid_to_ind[m] for m in AID_LOC[i]['members']]
+            new_dict["activity"] = AID_LOC[i]["activity"]
+            new_dict["pid"] = AID_LOC[i]["visitors"]
+            
+            ind_to_aid[original_loc_length + count] = i
+            
+            count+=1
+            
+            LOCATIONS.append(new_dict)
+        
+        LOCATIONS_agg.append(new_dict)
+    
+    CLIENT_LOCATIONS_agg = {}
+    for key in CLIENT_LOCATIONS.keys():
+        new_list = []
+        for loc in AID_CLIENT[key]:
+            if loc >= HOME_SHIFT:
+                new_list.insert(0, lid_to_ind[loc])
+            else:
+                new_list.append(LOCATIONS_agg[loc]['lid_ind'])
+        CLIENT_LOCATIONS_agg[key] = new_list
+    
+    return LOCATIONS_agg, CLIENT_LOCATIONS_agg
+
+#TODO: change datastructures
+def single_linkage_aggregation2(radius: float = 0.02):
+    """
+    Must be run after LOCATIONS and CLIENT_LOCATIONS are read in
+    
+    
+    Returns aggregated versions of LOCATIONS and CLIENT_LOCATIONS with single linkage clustering
+    """
+    
+    file_dict = open("aid2pid.json", 'r')
+    data_dict = json.load(file_dict)
+    AID_LOC = data_dict['data']
+
+    file_client = open("pid2aid.json", 'r')
+    data_client = json.load(file_client)
+    AID_CLIENT = {int(key):val for key,val in data_client['data'].items()}
+    
+    lid_to_ind = {}
+    for ind, val in enumerate(LOCATIONS):
+        lid_to_ind[val['lid']] = ind
+    
+    ind_to_aid = {}
+    LOCATIONS_agg = []
+    original_loc_length = len(LOCATIONS)
+    
+    count = 0
+    for i in range(len(AID_LOC)):
+        
+        if AID_LOC[i]['aid'] >= HOME_SHIFT:
+            new_dict = {}
+            new_dict["lid_ind"] = AID_LOC[i]['aid']
+            new_dict["lid"] = AID_LOC[i]['aid']
+            new_dict["longitude"] = AID_LOC[i]['longitude']
+            new_dict["latitude"] = AID_LOC[i]['latitude']
+            new_dict["members"] = [lid_to_ind[AID_LOC[i]['aid']]]
+            new_dict["activity"] = AID_LOC[i]["activity"]
+            new_dict["pid"] = AID_LOC[i]["visitors"]
+            
+            ind_to_aid[lid_to_ind[AID_LOC[i]['aid']]] = i
+        
+        else:
+            #lids of new aggregated locations are set to be negative
+            new_dict = {}
+            new_dict["lid_ind"] = AID_LOC[i]['aid']
+            new_dict["lid"] = -1 * (count+1)
+            new_dict["longitude"] = AID_LOC[i]['longitude']
+            new_dict["latitude"] = AID_LOC[i]['latitude']
+            new_dict["members"] = [lid_to_ind[m] for m in AID_LOC[i]['members']]
+            new_dict["activity"] = AID_LOC[i]["activity"]
+            new_dict["pid"] = AID_LOC[i]["visitors"]
+            
+            ind_to_aid[original_loc_length + count] = i
+            
+            count+=1
+            
+            LOCATIONS.append(new_dict)
+        
+        LOCATIONS_agg.append(new_dict)
+    
+    CLIENT_LOCATIONS_agg = {}
+    for key in CLIENT_LOCATIONS.keys():
+        new_list = []
+        for loc in AID_CLIENT[key]:
+            if loc >= HOME_SHIFT:
+                new_list.insert(0, lid_to_ind[loc])
+            else:
+                new_list.append(LOCATIONS_agg[loc]['lid_ind'])
+        CLIENT_LOCATIONS_agg[key] = new_list
+    
+    return LOCATIONS_agg, CLIENT_LOCATIONS_agg
+
+def aggregate_data(aggregation: int = 1, radius: float = 0.01):
+    """
+    Aggregation Options
+        0 : default, no aggregation
+        1 : set cover aggregation
+        2 : single linkage aggregation
+    """
     if aggregation == 2:
-        print("YIKES! Not yet implemented")
+        
+        return single_linkage_aggregation()
+        
     elif aggregation == 1:
+        
         filename = f"radius_cover_{radius}.json"
         if not path.exists(filename):
             radius_cover(radius)
-        return new_data_repeats()
+        return set_cover_aggregation()
+        
     else:
+        
         return LOCATIONS, CLIENT_LOCATIONS
 
-LOCATIONS_agg, CLIENT_LOCATIONS_agg = aggregate_data(1)
+
+def reverse_aggregation(aggreation: int, original_loc_length: int):
+    if aggregation == 2:
+        del LOCATIONS[original_loc_length:]
