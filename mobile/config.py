8b467c8004c465931f3f16a98a33f4eb2c487834
Ubuntu <ubuntu@ip-172-31-24-235.ec2.internal>
2021-08-18 19:55:23 +0000

    hi ann

--- mobile/config.py
+++ mobile/config.py
@@ -38,54 +38,7 @@ CLIENT_LOCATIONS_agg : Dict mapping pids of clients to the aggregated locations
 address = namedtuple('address', ['index', 'location', 'facility'])
 HOME_SHIFT=1000000000
 
-#OUTDATED
-"""def create_data_input():
-    #Read in both the activity and residence locations
-    df_activity = pd.read_csv("usa_va_charlottesville_city_activity_locations.csv").rename({"alid": "lid"}, axis = 'columns')
-    df_residence = pd.read_csv("usa_va_charlottesville_city_residence_locations.csv")
-
-    #Shift the residence lid
-    df_residence['lid'] = df_residence['rlid'] + HOME_SHIFT
-    locations = pd.concat([df_activity[['lid', 'longitude', 'latitude']], df_residence[['lid', 'longitude', 'latitude']]]).reset_index(drop = True)
-
-    #Read in the client visited locations data (adults only for now)
-    client_locations = pd.read_csv("usa_va_charlottesville_city_adult_activity_location_assignment_week.csv")
-
-    #Get the coordinates of all the residential locations
-    home_coords = set(df_residence[['latitude', 'longitude']].apply(tuple, axis=1).tolist())
-
-    #Shift lids for residential areas
-    client_locations['coord'] = client_locations[['latitude', 'longitude']].apply(lambda x: (x.latitude, x.longitude), axis = 1)
-    client_locations.loc[client_locations.coord.isin(home_coords), 'lid'] += HOME_SHIFT
-    
-    #Find popularity of locations, which locations are visited by which individuals
-    assignments = client_locations.copy()
-    assignments = assignments.groupby(['lid'])['pid'].apply(set).reset_index(name = 'pid')
-    assignments = assignments.set_index('lid')
-    assignments['activity'] = assignments['pid'].apply(lambda x: len(x))
-
-    locations['activity'] = locations['lid'].apply(lambda x: assignments.at[x, 'activity'] if x in assignments.index else 0)
-    #locations['pid'] = locations['lid'].applyg(lambda x: assignments.at[x, 'pid'] if x in assignments.index else {})
-    locations = locations.sort_values(by = 'activity', ascending = False).reset_index(drop = True)
-
-    client_locations = client_locations.groupby(['pid'])['lid'].apply(set).reset_index(name = 'lid')
-
-    #Replace lid with the index of the lid in locations
-    def filter(x):
-        return_list = []
-        for i in x:
-        #Insert home locations at the front of the list
-            if (i>HOME_SHIFT):
-                return_list.insert(0, locations.loc[locations.lid==i].index[0])
-            else:
-                return_list.append(locations.loc[locations.lid==i].index[0])
-        return return_list
-
-    client_locations['lid'] = client_locations['lid'].apply(lambda x: filter(x))
-
-    return locations.to_dict('index'), client_locations.to_dict('index')"""
-
-def create_data_input2():
+def create_data_input():
     """
     LOCATIONS contains the pid of clients that visit the location
     """
@@ -135,12 +88,12 @@ def create_data_input2():
     return locations.to_dict('index'), client_locations.to_dict('index')
 
 def write_data_input():
-    with open('data2.json', 'w') as f:
-        LOCATIONS, CLIENT_LOCATIONS = create_data_input2()
+    with open('original.json', 'w') as f:
+        LOCATIONS, CLIENT_LOCATIONS = create_data_input()
         data = {"LOCATIONS": LOCATIONS, "CLIENT_LOCATIONS": CLIENT_LOCATIONS}
         json.dump(data, f)
 
-def read_data_input(filename):
+def read_data_input(filename = 'original.json'):
     file = open(filename, 'r')
     data = json.load(file)
 
@@ -154,9 +107,12 @@ def read_data_input(filename):
 
 ######################################################
 
-LOCATIONS, CLIENT_LOCATIONS = read_data_input("data2.json")
+LOCATIONS, CLIENT_LOCATIONS = read_data_input()
 
 ######################################################
+
+LOCATIONS, CLIENT_LOCATIONS = [], {}
+
 
 def radius_cover(radius: float):
     """
@@ -199,7 +155,7 @@ def radius_cover(radius: float):
         cover = cover.union(max_choice[1])
         chosen.add(max_choice[2])
     
-    with open(f'radius_cover_{radius}.json', 'w') as f:
+    with open(f'radius_cover_{1000*radius}.json', 'w') as f:
         data = {"radius": radius, "radius_dict": radius_dict, "chosen": list(chosen)}
         json.dump(data, f)
 
@@ -462,7 +418,6 @@ def aggregate_data(aggregation: int = 1, radius: float = 0.01):
     """
     
     if aggregation == 2:
-        print("NOT IMPLEMENTED")
         return single_linkage_aggregation2()
         
     elif aggregation == 1:
